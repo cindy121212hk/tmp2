227b9339566737f88c99fad8d11341d806921c4f
cindy121212hk <cindy121212hk@yahoo.com.hk>
2023-07-12 09:41:36 +0800

    again

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const logger = require('./logger');
 const os = require('os');
 const os = require('fs');
+const _ = require('lodash');
 
 function sayHello(name){
     console.log("Hello " + name + logger.loggername);
